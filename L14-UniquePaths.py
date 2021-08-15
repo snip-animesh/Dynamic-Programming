162485219fc7ea0ada437a753259f4a6a8c5e362
snip-animesh <83237705+snip-animesh@users.noreply.github.com>
2021-08-15 14:36:24 +0600

    Update L14-UniquePaths.py

--- L14-UniquePaths.py
+++ L14-UniquePaths.py
@@ -34,3 +34,30 @@ print(dp)
 
 # Logic from CodeNCode DP series L-14. 
 # Practice Problem https://leetcode.com/problems/unique-paths/   and  https://leetcode.com/problems/unique-paths-ii/
+
+"""UNIQUE PATH WITH OBSTACLES"""
+# Practice Problem --> https://leetcode.com/problems/unique-paths-ii/
+grid=[[0,0,0],[0,1,0],[0,0,0]]
+m,n=len(grid),len(grid[0])
+if grid[m-1][n-1]==1:
+    print (0)
+else:
+    grid[m-1][n-1]=1
+    for i in range(n-2,-1,-1):
+        if grid[m-1][i]==0 and grid[m-1][i+1]==1:
+            grid[m-1][i]=1
+        else:
+            grid [m-1][i]=0
+    for i in range(m-2,-1,-1):
+        if grid[i][n-1]==0 and grid[i+1][n-1]==1:
+            grid[i][n-1]=1
+        else:
+            grid[i][n-1]=0
+
+    for i in range(m-2,-1,-1):
+        for j in range(n-2,-1,-1):
+            if not grid[i][j]:
+                grid[i][j]=grid[i+1][j]+grid[i][j+1]
+            else:
+                grid[i][j]=0
+    print(grid[0][0])
