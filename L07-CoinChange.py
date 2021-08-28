1c69851258a1102d4b24c0e7db9551190bbd5554
snip-animesh <83237705+snip-animesh@users.noreply.github.com>
2021-08-28 23:25:14 +0600

    Update L07-CoinChange.py

--- L07-CoinChange.py
+++ L07-CoinChange.py
@@ -4,15 +4,17 @@ amount=int(input())
 dp=[10**8]*(amount+1)
 dp[0]=0
 for i in range(1,amount+1):
-    x=-1
+    x=10**8
     for j in coins:
         if j<=i:
-            x=min(dp[i],dp[i-j])
+            x=min(x,dp[i-j])
         else:
             break
-    if x==-1:
+    if x==10**8:
         dp[i]=x
     else:
         dp[i]=x+1
-
-print(dp[amount])
+if dp[amount]==10**8:
+    print(-1)
+else:
+    print(dp[amount])
